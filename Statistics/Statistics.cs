0b715243690ecf64e760d451c32ce85758d25b93
Ramandeep Singh (MS/ECI-XC) <Ramandeep.Singh2@in.bosch.com>
2022-05-21 21:59:24 +0530

    Test function 1

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,27 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public float average;
+        public float max;
+        public float min;
+
+        public Stats CalculateStatistics(List<float> numbers)
+        {
+            float sum = 0;
+            float max = float.MinValue;
+            float min = float.MaxValue;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+
+                if (numbers[i] > max)
+                    max = numbers[i];
+
+                if (numbers[i] < min)
+                    min = numbers[i];
+            }
+
+            return new Stats();
         }
     }
 }
